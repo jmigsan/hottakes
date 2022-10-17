cf4826f2284da10678a9e74fdf40c200e2bf4072
jmigsan <43961001+jmigsan@users.noreply.github.com>
2022-10-17 14:04:44 +0100

    prisma stuff and home buttons

--- src/components/Index/Categorybar.tsx
+++ src/components/Index/Categorybar.tsx
@@ -1,15 +1,22 @@
-import { Box, Button, Center, HStack } from '@chakra-ui/react';
+import { Box, Button, Center, HStack, SimpleGrid } from '@chakra-ui/react';
 
 const Categorybar = () => {
   return (
     <Box>
       <Center p={'1'}>
-        <HStack>
+        {/* <HStack display={{ base: 'none', md: 'inline' }}>
           <Button>Most Liked</Button>
           <Button>Most Disliked</Button>
           <Button>Most Controversial</Button>
           <Button>Least Controversial</Button>
-        </HStack>
+        </HStack> */}
+        <SimpleGrid columns={2} spacing={10}>
+          <Box bg='tomato' height='80px'></Box>
+          <Box bg='tomato' height='80px'></Box>
+          <Box bg='tomato' height='80px'></Box>
+          <Box bg='tomato' height='80px'></Box>
+          <Box bg='tomato' height='80px'></Box>
+        </SimpleGrid>
       </Center>
     </Box>
   );
