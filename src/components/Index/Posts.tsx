27a0ee1a8e90f414e14047e2a2d38a5d1ec2930b
jmigsan <43961001+jmigsan@users.noreply.github.com>
2022-10-17 18:23:39 +0100

    sidebar and router

--- src/components/Index/Posts.tsx
+++ src/components/Index/Posts.tsx
@@ -1,28 +1,24 @@
 import { Box, Center, Image, Stack, Text } from '@chakra-ui/react';
+import { trpc } from '../../utils/trpc';
 
 const Posts = () => {
-  return (
+  const posts = trpc.return(
     <Box py={'3'}>
       <Stack>
         <Box>
           <Center>
             <Stack>
-              <Box>
-                <Image
-                  src='https://images.unsplash.com/photo-1665865553329-fc8a48ca8584?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80'
-                  boxSize='md'
-                  objectFit='cover'
-                />
-                <Text>yo</Text>
-              </Box>
-              <Box>
-                <Image
-                  src='https://images.unsplash.com/photo-1665873364045-74b357e96d2a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
-                  boxSize='md'
-                  objectFit='cover'
-                />
-                <Text>oi</Text>
-              </Box>
+              {allPosts.data?.map((post) => (
+                <Link href={`/post/${post.postId}`} key={post.postId}>
+                  <Box bg={'gray.200'} p={3} rounded={'lg'}>
+                    <>
+                      <Text fontSize={'2xl'}>{post.title}</Text>
+                      <Text>{post.publishDate?.toDateString()}</Text>
+                      <Text>{post.author}</Text>
+                    </>
+                  </Box>
+                </Link>
+              ))}
             </Stack>
           </Center>
         </Box>
--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,11 +1,13 @@
 // src/server/trpc/router/_app.ts
-import { router } from "../trpc";
-import { exampleRouter } from "./example";
-import { authRouter } from "./auth";
+import { router } from '../trpc';
+import { exampleRouter } from './example';
+import { authRouter } from './auth';
+import { postRouter } from './post';
 
 export const appRouter = router({
   example: exampleRouter,
   auth: authRouter,
+  post: postRouter,
 });
 
 // export type definition of API
