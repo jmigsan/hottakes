2bfec0fae83ad5854f24145f8113877a4f70b064
jmigsan <43961001+jmigsan@users.noreply.github.com>
2022-10-17 17:38:14 +0100

    pub route change

--- src/server/trpc/router/public-router.ts
+++ src/server/trpc/router/public-router.ts
@@ -1,38 +1,8 @@
-import { createRouter } from './context';
+import { router, publicProcedure } from '../trpc';
 import { z } from 'zod';
 
-export const publicRouter = createRouter()
-  .query('getPost', {
-    input: z.object({
-      postId: z.string(),
-    }),
-    async resolve({ ctx, input }) {
-      return await ctx.prisma.post.findFirst({
-        where: { postId: input.postId },
-      });
-    },
-  })
-  .query('getPosts', {
-    async resolve({ ctx }) {
-      return await ctx.prisma.post.findMany({
-        where: { published: true },
-      });
-    },
-  })
-  .query('getProduct', {
-    input: z.object({
-      productId: z.string(),
-    }),
-    async resolve({ ctx, input }) {
-      return await ctx.prisma.product.findFirst({
-        where: { productId: input.productId },
-      });
-    },
-  })
-  .query('getProducts', {
-    async resolve({ ctx }) {
-      return await ctx.prisma.product.findMany({
-        where: { published: true },
-      });
-    },
-  });
+export const publicRouter = router({
+  getPosts: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.post.findMany();
+  }),
+});
