e44b0a213bb8eb33a77a5bb3226607b3228d3e12
jmigsan <43961001+jmigsan@users.noreply.github.com>
2022-11-10 23:11:42 +0000

    adding clarification if category bar works

--- src/components/Index/Categorybar.tsx
+++ src/components/Index/Categorybar.tsx
@@ -8,27 +8,98 @@ import {
   Show,
   SimpleGrid,
   Text,
+  useToast,
 } from '@chakra-ui/react';
 
 const Categorybar = () => {
+  const toast = useToast();
+
+  const mostLiked = () => {
+    toast({
+      title: 'Feature not implemented yet',
+      description: 'Would sort by most likes',
+      status: 'error',
+      duration: 9000,
+      isClosable: true,
+    });
+  };
+
+  const mostDisliked = () => {
+    toast({
+      title: 'Feature not implemented yet',
+      description: 'Would sort by most dislikes',
+      status: 'error',
+      duration: 9000,
+      isClosable: true,
+    });
+  };
+
+  const mostControversial = () => {
+    toast({
+      title: 'Feature not implemented yet',
+      description: 'Would sort by lowest ratio of likes to dislikes',
+      status: 'error',
+      duration: 9000,
+      isClosable: true,
+    });
+  };
+
+  const leastControversial = () => {
+    toast({
+      title: 'Feature not implemented yet',
+      description: 'Would sort by highest ratio of likes to dislikes',
+      status: 'error',
+      duration: 9000,
+      isClosable: true,
+    });
+  };
+
   return (
     <>
       <Center>
         <Show above='md'>
           <HStack>
-            <Button colorScheme={'blackAlpha'}>Most Liked</Button>
-            <Button colorScheme={'blackAlpha'}>Most Disliked</Button>
-            <Button colorScheme={'blackAlpha'}>Most Controversial</Button>
-            <Button colorScheme={'blackAlpha'}>Least Controversial</Button>
+            <Button colorScheme={'blackAlpha'} onClick={() => mostLiked()}>
+              Most Liked
+            </Button>
+            <Button colorScheme={'blackAlpha'} onClick={() => mostDisliked()}>
+              Most Disliked
+            </Button>
+            <Button
+              colorScheme={'blackAlpha'}
+              onClick={() => mostControversial()}
+            >
+              Most Controversial
+            </Button>
+            <Button
+              colorScheme={'blackAlpha'}
+              onClick={() => leastControversial()}
+            >
+              Least Controversial
+            </Button>
           </HStack>
         </Show>
       </Center>
       <Show below='md'>
         <SimpleGrid columns={2} spacing={2}>
-          <Button colorScheme={'blackAlpha'}>Most Liked</Button>
-          <Button colorScheme={'blackAlpha'}>Most Disliked</Button>
-          <Button colorScheme={'blackAlpha'}>Most Controversial</Button>
-          <Button colorScheme={'blackAlpha'}>Least Controversial</Button>
+          <Button colorScheme={'blackAlpha'} onClick={() => mostLiked()}>
+            Most Liked
+          </Button>
+          <Button colorScheme={'blackAlpha'} onClick={() => mostDisliked()}>
+            Most Disliked
+          </Button>
+          <Button
+            colorScheme={'blackAlpha'}
+            onClick={() => mostControversial()}
+          >
+            Most Controversial
+          </Button>
+          <Button
+            colorScheme={'blackAlpha'}
+            onClick={() => leastControversial()}
+          >
+            Least Controversial
+          </Button>
         </SimpleGrid>
       </Show>
     </>
